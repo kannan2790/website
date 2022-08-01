26632fb2d9205dd792a38958f284369324dbc507
kannan2790 <kannan2790@yahoo.com>
2022-08-01 16:34:16 -0400

    Create terra.tf

---
+++ terra.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+region = "us-east-1"
+access_key="AKIA53CDTEZHZNHK7TTJ"
+secret_key="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"
+}
+
+resource "aws_instance" "k8s-main" {
+ami="ami-08d4ac5b634553e16"
+instance_type="t2.small"
+security_groups=["launch-wizard-9"]
+key_name="pem"
+provisioner "local-exec" {
+
+command = "echo ${aws_instance.k8s-main.public_ip}"
+}
+}
+resource "aws_instance" "k8s-work1" {
+ami="ami-08d4ac5b634553e16"
+instance_type="t2.micro"
+security_groups=["launch-wizard-9"]
+key_name="pem"
+provisioner "local-exec" {
+
+command = "echo ${aws_instance.k8s-work1.public_ip}"
+}
+}
+
+resource "aws_instance" "k8s-work2" {
+ami="ami-08d4ac5b634553e16"
+instance_type="t2.micro"
+security_groups=["launch-wizard-9"]
+key_name="pem"
+provisioner "local-exec" {
+
+command = "echo ${aws_instance.k8s-work2.public_ip}"
+}
+}
+
